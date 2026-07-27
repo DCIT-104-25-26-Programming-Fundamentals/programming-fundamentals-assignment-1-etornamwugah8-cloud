e734d8b7bd398720bf0bae680943dbd9ac169ff5
etornamwugah8-cloud <etornamwugah8@gmail.com>
2026-07-27 20:42:44 +0000

    Implement prime checking function

--- assignment_01_prime_checker.py
+++ assignment_01_prime_checker.py
@@ -34,4 +34,21 @@
 # =============================================================================
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
+def is_prime(num):
+  if num < 2:
+    return False
 
+for i in range(2, num):
+  if num % i == 0:
+    return False
+
+return True
+
+
+number = int(input("Enter a number: "))
+
+if is_prime(number):
+  print(f"{number} is a prime number.")
+else:
+  print(f"{number} is Not a prime number.")
+    
